5954980451733f4872fd72b701cd160e9dd3897a
MyClaudputer <myclaudputer@MyClaudtersMini.home>
2026-02-08 04:20:02 -0800

    fix: use standard firebase-admin import

--- functions/src/firebase-admin.js
+++ functions/src/firebase-admin.js
@@ -1,9 +1,7 @@
-import * as admin from 'firebase-admin';
+import admin from 'firebase-admin';
 
-// Firebase Admin SDK is automatically initialized in Cloud Functions runtime
-// No need to initialize it again
+// In Cloud Functions, the Admin SDK initializes automatically
 const db = admin.firestore();
 const auth = admin.auth();
 
 export { db, auth };
-export default admin;
